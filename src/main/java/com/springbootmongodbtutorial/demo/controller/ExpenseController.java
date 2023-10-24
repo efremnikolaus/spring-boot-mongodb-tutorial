8e2f382bf8d8e1508114e1679c027c924ac07eba
Nikolai Efremov <efremovnikolaus@gmail.com>
2023-10-24 20:09:07 +0200

    added basic func for CRUD service

---
+++ src/main/java/com/springbootmongodbtutorial/demo/controller/ExpenseController.java
@@ -0,0 +1,48 @@
+package com.springbootmongodbtutorial.demo.controller;
+
+import com.springbootmongodbtutorial.demo.model.Expense;
+import com.springbootmongodbtutorial.demo.model.ExpenseCategory;
+import com.springbootmongodbtutorial.demo.service.ExpenseService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/expense")
+public class ExpenseController {
+    private final ExpenseService expenseService;
+
+    public ExpenseController(ExpenseService expenseService) {
+        this.expenseService = expenseService;
+    }
+
+    @PostMapping
+    public ResponseEntity addExpense(@RequestBody Expense expense) {
+        expenseService.addExpense(expense);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PutMapping
+    public ResponseEntity updateExpense(@RequestBody Expense expense) {
+        expenseService.updateExpense(expense);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    @GetMapping("/{name}")
+    public ResponseEntity getExpenseByName(@PathVariable String name) {
+        return ResponseEntity.ok(expenseService.getExpense(name));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Expense>> getAllExpenses() {
+        return ResponseEntity.ok(expenseService.getAllExpenses());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity deleteExpense(@PathVariable String id) {
+        expenseService.deleteExpense(id);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ src/main/java/com/springbootmongodbtutorial/demo/service/ExpenseService.java
@@ -0,0 +1,46 @@
+package com.springbootmongodbtutorial.demo.service;
+
+import com.springbootmongodbtutorial.demo.model.Expense;
+import com.springbootmongodbtutorial.demo.repository.ExpenseRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+public class ExpenseService {
+    private final ExpenseRepository expenseRepository;
+
+    public ExpenseService(ExpenseRepository expenseRepository) {
+        this.expenseRepository = expenseRepository;
+    }
+
+    public void addExpense(Expense expense) {
+        expenseRepository.insert(expense);
+    }
+
+    public void updateExpense(Expense expense) {
+        Expense savedExpense = expenseRepository.findById(expense.getId())
+                .orElseThrow(() -> new RuntimeException(String.format("Cannot Find Expense by ID %s", expense.getId())));
+
+        savedExpense.setExpenseName(expense.getExpenseName());
+        savedExpense.setExpenseCategory(expense.getExpenseCategory());
+        savedExpense.setExpenseAmount(expense.getExpenseAmount());
+        expenseRepository.save(expense);
+    }
+
+    public Expense getExpense(String name) {
+        return expenseRepository.findByName(name)
+                .orElseThrow(() -> new RuntimeException(String.format("Cannot find expense by name: %s", name)));
+    }
+
+    public List<Expense> getAllExpenses() {
+        return expenseRepository.findAll();
+    }
+
+    public void deleteExpense(String id) {
+        expenseRepository.deleteById(id);
+    }
+}
