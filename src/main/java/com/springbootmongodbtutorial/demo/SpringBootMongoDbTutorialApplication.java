7287bfbc48552892a8277fcc462cf324f9fc5503
Nikolai Efremov <efremovnikolaus@gmail.com>
2023-10-24 19:21:49 +0200

    Initial commit, new project

---
+++ src/main/java/com/springbootmongodbtutorial/demo/SpringBootMongoDbTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.springbootmongodbtutorial.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMongoDbTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMongoDbTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbootmongodbtutorial/demo/SpringBootMongoDbTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootmongodbtutorial.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMongoDbTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
